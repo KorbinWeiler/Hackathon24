da29c48321e17db562d80d7839749a42cb9b06b5
KorbinWeiler <korbinweiler@gmail.com>
2024-02-17 13:27:28 -0800

    Add gui window Add makeButtons function

--- gui.py
+++ gui.py
@@ -1,2 +1,23 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
+#window setup
+root = Tk(className="Menu")
+root.config(bg='#08103d')
+root.geometry("512x512")
+root.grid()
+
+def onClick(position):
+    print("botton!")
+    return
+
+def makeButtons(size):
+    buttons = []
+    for i in range(size):
+        for j in range(size):
+            button = ttk.Button(root, text=str((i * 10)-((10-size) * i)+j), width=10, command=lambda i=i: onClick(i)).grid(column=j,row=i)
+            buttons.append(button)
+    return
+
+makeButtons(5)
+root.mainloop()
