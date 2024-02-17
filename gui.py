e67dc4bb42ff6b04c6daf2a57d6b5c2effcbe137
KorbinWeiler <korbinweiler@gmail.com>
2024-02-17 14:47:02 -0800

    bug fixes

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
+from math import floor
 
 #window setup
 root = Tk(className="Menu")
@@ -11,13 +13,24 @@ def onClick(position):
     print("botton!")
     return
 
-def makeButtons(size):
+def makeButtons(height, width):
+
+    #list of buttons
     buttons = []
-    for i in range(size):
-        for j in range(size):
-            button = ttk.Button(root, text=str((i * 10)-((10-size) * i)+j), width=10, command=lambda i=i: onClick(i)).grid(column=j,row=i)
-            buttons.append(button)
+    buttonSize = root.winfo_geometry
+    print(buttonSize)
+
+    #creates size * size buttons
+    for i in range(width):
+        for j in range(height):
+            button = tk.Button(root, text=str((i * 10)-((10-height) * i)+j), height = 2, width=floor(root.winfo_width() * width),
+                                command=lambda i=i: onClick(i))
+            button.grid(column=j,row=i, padx=1, pady=1)
+            button.config(background="dark blue", fg = 'white')
+            
+            #adds new button to the list
+            buttons.append(button)      
     return
 
-makeButtons(5)
+makeButtons(5, 10)
 root.mainloop()
